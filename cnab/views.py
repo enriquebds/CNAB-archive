bfe6b23196ee6201f85b3b3924f60b85b75e8c51
Enrique Barbosa da Silva <enrique.barbosasilva@gmail.com>
2023-01-25 15:21:12 -0300

    Update views.py

--- cnab/views.py
+++ cnab/views.py
@@ -15,5 +15,5 @@ def upload(req):
 
         list = []
         with open(f"uploads/{str(archive.archive)}", encoding="utf-8") as read:
-             for file_line in read:
-                list.append(file_line)
+             for line in read:
+                list.append(line)
